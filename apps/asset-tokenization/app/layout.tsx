eede4c11c05a269667041ba8f2b39406caa16944
Roderik van der Veer <roderik@settlemint.com>
2024-08-19 20:50:38 +0200

    fix

--- apps/asset-tokenization/app/layout.tsx
+++ apps/asset-tokenization/app/layout.tsx
@@ -50,8 +50,11 @@ export default function RootLayout({
               <main>{children}</main>
               <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
                 <p className="text-xs text-muted-foreground">
-                  &copy; 2024 SettleMint. Freely available under the Functional Source License, Version 1.1, MIT Future
-                  License.
+                  &copy; {new Date().getFullYear()}{" "}
+                  <Link href="https://settlemint.com" className="hover:underline" prefetch={false}>
+                    SettleMint
+                  </Link>
+                  . Freely available under the Functional Source License, Version 1.1, MIT Future License.
                 </p>
                 <nav className="sm:ml-auto flex gap-4 sm:gap-6">
                   <Link
