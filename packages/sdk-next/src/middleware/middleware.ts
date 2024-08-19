ea92f1f3019c4c6126aa6947907325a62e26870c
Roderik van der Veer <roderik@settlemint.com>
2024-08-19 17:08:26 +0200

    fix

--- packages/sdk-next/src/middleware/middleware.ts
+++ packages/sdk-next/src/middleware/middleware.ts
@@ -3,14 +3,30 @@ import { NextResponse } from "next/server.js";
 import { createRouteMatcher } from "./lib/route-matcher.js";
 import { getSession } from "./session/session.js";
 
-const isAuthenticatedRoute = createRouteMatcher(["/app/(.*)"]);
+const isProxyRoute = createRouteMatcher(["/proxy/(.*)"]);
+const isHasuraProxyRoute = createRouteMatcher(["/proxy/hasura"]);
+const isAuthenticatedRoute = createRouteMatcher(["/s", "/s/(.*)"]);
+const isAuthRoute = createRouteMatcher(["/a", "/a/(.*)"]);
 
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
+
+  if (isProxyRoute(request)) {
+    response.headers.delete("Authorization");
+    response.headers.set("x-auth-token", process.env.SETTLEMINT_PAT ?? "");
+    if (isHasuraProxyRoute(request)) {
+      response.headers.set("x-hasura-admin-secret", process.env.SETTLEMINT_HASURA_ADMIN_SECRET ?? "");
+    }
+    return response;
+  }
+
   const session = await getSession(request, response);
 
   if (isAuthenticatedRoute(request) && !session.auth?.address) {
-    return NextResponse.redirect(new URL(`/auth/login?rd=${request.nextUrl.pathname}`, request.nextUrl.origin));
+    return NextResponse.redirect(new URL(`/a?rd=${request.nextUrl.pathname}`, request.nextUrl.origin));
+  }
+  if (isAuthRoute(request) && session.auth?.address) {
+    return NextResponse.redirect(new URL("/s", request.nextUrl.origin));
   }
 
   return response;
