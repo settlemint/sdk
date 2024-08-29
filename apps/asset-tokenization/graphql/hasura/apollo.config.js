a71ae7b916639fdfad3930ea78092a170782095b
Roderik van der Veer <roderik@settlemint.com>
2024-08-29 07:15:32 +0200

    wip

--- apps/asset-tokenization/graphql/hasura/apollo.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  client: {
-    includes: ["./**/*.graphql", "./*.graphql"],
-    service: {
-      name: "settlemint-hasura",
-      localSchemaFile: "../../.settlemint/hasura/gql/schema.graphql",
-    },
-  },
-};
--- apps/asset-tokenization/graphql/portal/apollo.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  client: {
-    includes: ["./**/*.graphql", "./*.graphql"],
-    service: {
-      name: "settlemint-portal",
-      localSchemaFile: "../../.settlemint/portal/gql/schema.graphql",
-    },
-  },
-};
