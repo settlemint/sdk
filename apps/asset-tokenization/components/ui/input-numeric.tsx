12e39009d09ad990358fd1344b25ab9e0e80f9d7
Patrick Mualaba <patrick.mualaba@live.be>
2024-09-03 18:16:06 +0200

    fix formpage validation

--- apps/asset-tokenization/components/ui/input-numeric.tsx
+++ apps/asset-tokenization/components/ui/input-numeric.tsx
@@ -5,27 +5,34 @@ import { Input } from "./input";
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const NumericInput = React.forwardRef<HTMLInputElement, InputProps>(({ className, name = "", placeholder = "" }) => {
-  return (
-    <Controller
-      name={name}
-      render={({ field }) => (
-        <Input
-          {...field}
-          name={field.name}
-          type="number"
-          placeholder={placeholder}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            const value = e.target.value === "" ? "" : Number(e.target.value);
-            field.onChange(value);
-          }}
-          value={field.value === "" ? "" : Number(field.value)}
-          className={className}
-        />
-      )}
-    />
-  );
-});
+const NumericInput = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, name = "", placeholder = "", ...props }, ref) => {
+    return (
+      <Controller
+        name={name}
+        render={({ field }) => {
+          const _value = field.value === "" ? 0 : Number(field.value);
+          return (
+            <Input
+              {...field}
+              {...props}
+              ref={ref}
+              name={field.name}
+              type="number"
+              placeholder={placeholder}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                const value = e.target.value === "" ? "" : Number(e.target.value);
+                field.onChange(value);
+              }}
+              value={Number.isNaN(_value) ? 0 : _value}
+              className={className}
+            />
+          );
+        }}
+      />
+    );
+  },
+);
 NumericInput.displayName = "NumericInput";
 
 export { NumericInput };
