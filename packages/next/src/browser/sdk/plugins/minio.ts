9a73c3054bff3dfe8f2f97cc36f1d7abe794d2f3
Roderik van der Veer <roderik@settlemint.com>
2024-10-01 15:22:41 +0200

    fix: optional minio

--- packages/next/src/browser/sdk/plugins/minio.ts
+++ packages/next/src/browser/sdk/plugins/minio.ts
@@ -1,8 +1,11 @@
 import { Client } from "minio";
 
-export function createMinioS3Client({ endPoint }: { endPoint: string }) {
+export function createMinioS3Client(options?: { endPoint?: string }) {
+  if (!options?.endPoint || !process.env.SETTLEMINT_MINIO_ACCESS_KEY || !process.env.SETTLEMINT_MINIO_SECRET_KEY) {
+    return null;
+  }
   return new Client({
-    endPoint: endPoint,
+    endPoint: options.endPoint,
     port: 443,
     useSSL: true,
     accessKey: process.env.SETTLEMINT_MINIO_ACCESS_KEY ?? "",
