f9ff2f955df808363e92279eec4b86911408cffd
Roderik van der Veer <roderik@settlemint.com>
2024-10-04 13:43:49 +0200

    fix: build export

--- packages/js/tsup.config.ts
+++ packages/js/tsup.config.ts
@@ -17,9 +17,5 @@ export default defineConfig(({ watch }) => {
       entry: ["src/settlemint.ts"],
       ...sharedConfig,
     },
-    {
-      entry: ["src/schemas/schemas.ts"],
-      ...sharedConfig,
-    },
   ];
 });
