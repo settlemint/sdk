6d538f584e6023173d7764b7fad637bc6670af47
Roderik van der Veer <roderik@settlemint.com>
2024-10-09 18:11:06 +0200

    fix: allow paths

--- packages/utils/src/validation/dot-env.schema.ts
+++ packages/utils/src/validation/dot-env.schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { AccessTokenSchema } from "./access-token.schema.js";
 import { IdSchema } from "./id.schema.js";
-import { UrlSchema } from "./url.schema.js";
+import { UrlOrPathSchema, UrlSchema } from "./url.schema.js";
 /**
  * Schema for validating access tokens.
  */
@@ -11,13 +11,13 @@ export const DotEnvSchema = z.object({
   SETTLEMINT_WORKSPACE: IdSchema,
   SETTLEMINT_APPLICATION: IdSchema,
   SETTLEMINT_HASURA: IdSchema.optional(),
-  SETTLEMINT_HASURA_ENDPOINT: UrlSchema.optional(),
+  SETTLEMINT_HASURA_ENDPOINT: UrlOrPathSchema.optional(),
   SETTLEMINT_HASURA_ADMIN_SECRET: z.string().optional(),
   SETTLEMINT_THEGRAPH: IdSchema.optional(),
-  SETTLEMINT_THEGRAPH_SUBGRAPH_ENDPOINT: UrlSchema.optional(),
-  SETTLEMINT_THEGRAPH_SUBGRAPH_ENDPOINT_FALLBACK: UrlSchema.optional(),
+  SETTLEMINT_THEGRAPH_SUBGRAPH_ENDPOINT: UrlOrPathSchema.optional(),
+  SETTLEMINT_THEGRAPH_SUBGRAPH_ENDPOINT_FALLBACK: UrlOrPathSchema.optional(),
   SETTLEMINT_PORTAL: IdSchema.optional(),
-  SETTLEMINT_PORTAL_GRAPHQL_ENDPOINT: UrlSchema.optional(),
+  SETTLEMINT_PORTAL_GRAPHQL_ENDPOINT: UrlOrPathSchema.optional(),
   SETTLEMINT_PORTAL_REST_ENDPOINT: UrlSchema.optional(),
   SETTLEMINT_HD_PRIVATE_KEY: z.string().optional(),
   SETTLEMINT_MINIO: IdSchema.optional(),
--- packages/utils/src/validation/url.schema.ts
+++ packages/utils/src/validation/url.schema.ts
@@ -4,3 +4,21 @@ import { z } from "zod";
  */
 export const UrlSchema = z.string().url();
 export type Url = z.infer<typeof UrlSchema>;
+
+/**
+ * Schema for validating URL paths.
+ *
+ * This schema ensures that the path:
+ * - Starts with a forward slash
+ * - Can contain letters, numbers, hyphens, underscores, and additional forward slashes
+ * - Does not end with a forward slash (unless it's the root path "/")
+ * - Is case-sensitive
+ */
+export const UrlPathSchema = z.string().regex(/^\/(?:[a-zA-Z0-9-_]+(?:\/[a-zA-Z0-9-_]+)*\/?)?$/, {
+  message: "Invalid URL path format. Must start with '/' and can contain letters, numbers, hyphens, and underscores.",
+});
+
+export type UrlPath = z.infer<typeof UrlPathSchema>;
+
+export const UrlOrPathSchema = z.union([UrlSchema, UrlPathSchema]);
+export type UrlOrPath = z.infer<typeof UrlOrPathSchema>;
