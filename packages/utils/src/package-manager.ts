f1b517ef24a0ce19628fb30223934f4cc5bc0c3f
Roderik van der Veer <roderik@settlemint.com>
2024-10-14 15:36:23 +0200

    feat: add create back

--- packages/utils/src/package-manager.ts
+++ packages/utils/src/package-manager.ts
@@ -1,2 +1,4 @@
+export * from "./package-manager/download-and-extract.js";
 export { getPackageManager } from "./package-manager/get-package-manager.js";
 export { installDependencies } from "./package-manager/install-dependencies.js";
+export { setName } from "./package-manager/set-name.js";
