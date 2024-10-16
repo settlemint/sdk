afbd4de860cfbcedef13f01f5c1e8703dac19333
Roderik van der Veer <roderik@settlemint.com>
2024-10-16 18:43:30 +0200

    fix: colors

--- apps/registry/app/page.tsx
+++ apps/registry/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <ThemeToggle />
       </div>
       <div>
-        <div className="mt-8 mb-8 border border-gray-200 rounded-md p-4">
+        <div className="mt-8 mb-8 border border-gray-200 dark:border-gray-700 rounded-md p-4">
           <h2 className="text-lg font-bold">Collapsed Breadcrumb</h2>
           <div className="mt-8 mb-8">
             <CollapsedBreadcrumb
@@ -29,22 +29,22 @@ export default function Home() {
             />
           </div>
           <div className="mt-8 mb-8">
-            <pre className="bg-gray-100 p-3 rounded-md overflow-x-auto">
-              <code className="text-xs">
+            <pre className="bg-gray-100 dark:bg-gray-800 p-3 rounded-md overflow-x-auto">
+              <code className="text-xs text-gray-800 dark:text-gray-200">
                 bunx shadcn@latest add https://console.settlemint.com/registry/collapsed-breadcrumb.json
               </code>
             </pre>
           </div>
         </div>
-        <div className="mt-8 mb-8 border border-gray-200 rounded-md p-4">
+        <div className="mt-8 mb-8 border border-gray-200 dark:border-gray-700 rounded-md p-4">
           <h2 className="text-lg font-bold">Theme toggle</h2>
           <p className="text-sm text-muted-foreground mb-2">Wrap your topmost layout in a ThemeProvider</p>
           <div className="mt-8 mb-8">
             <ThemeToggle />
           </div>
           <div className="mt-8 mb-8">
-            <pre className="bg-gray-100 p-3 rounded-md overflow-x-auto">
-              <code className="text-xs">
+            <pre className="bg-gray-100 dark:bg-gray-800 p-3 rounded-md overflow-x-auto">
+              <code className="text-xs text-gray-800 dark:text-gray-200">
                 bunx shadcn@latest add https://console.settlemint.com/registry/dark-mode.json
               </code>
             </pre>
