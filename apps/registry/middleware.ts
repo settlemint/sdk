cb8b18c1abb0f705c9f5a07d8fd9b0b41426c400
Roderik van der Veer <roderik@settlemint.com>
2024-10-18 13:59:30 +0200

    feat: add middleware

---
+++ apps/registry/middleware.ts
@@ -0,0 +1,38 @@
+import { proxyMiddleware } from "@settlemint/sdk-next/middlewares/proxy";
+import NextAuth from "next-auth";
+import { NextResponse } from "next/server";
+import { match } from "path-to-regexp";
+import { authConfig } from "./lib/auth-config";
+
+const isUserAuthenticatedRoute = match(["/wallet", "/wallet/*path"]);
+
+const { auth } = NextAuth({
+  ...authConfig,
+  providers: [], // we don't want to import bcryptjs in the middleware
+});
+
+export default auth((request) => {
+  const proxyResponse = proxyMiddleware(request);
+  if (proxyResponse) {
+    return proxyResponse;
+  }
+
+  if (isUserAuthenticatedRoute(request.nextUrl.pathname) && !request.auth) {
+    return NextResponse.redirect(new URL("/auth/signin", request.url));
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
