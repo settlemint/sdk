79e10f11a124d82e7b99f855feb0729a3b4572cb
Roderik van der Veer <roderik@settlemint.com>
2024-10-19 12:22:39 +0200

    fix

--- apps/registry/next.config.mjs
+++ apps/registry/next.config.mjs
@@ -3,6 +3,9 @@ import { withSettleMint } from "@settlemint/sdk-next/config/with-settlemint";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding", "debug");
     config.resolve.fallback = { fs: false, net: false, tls: false };
