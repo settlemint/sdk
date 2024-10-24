90c8fa1d592750d158e5b148322c82afb70571b9
Roderik van der Veer <roderik@settlemint.com>
2024-10-24 17:08:22 +0200

    fix: address avatar does not work serverside

--- apps/registry/src/components/blocks/address-avatar/address-avatar.tsx
+++ apps/registry/src/components/blocks/address-avatar/address-avatar.tsx
@@ -1,25 +1,21 @@
-import { Avatar, AvatarImage } from "@/components/ui/avatar";
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
-import { cva } from "class-variance-authority";
-import type { HTMLAttributes } from "react";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { type HTMLAttributes, forwardRef, useState } from "react";
 import { getGravatarUrl } from "react-awesome-gravatar";
 import { http, type Address, createPublicClient } from "viem";
 import { mainnet } from "viem/chains";
-import { AddressAvatarIndicator } from "./address-avatar-indicator";
 
-const addressAvatarVariants = cva("rounded-md", {
-  variants: {
-    variant: {
-      tiny: "w-4 h-4 rounded-full",
-      small: "w-6 h-6 rounded-sm",
-      big: "w-10 h-10 rounded-md",
-    },
-  },
-  defaultVariants: {
-    variant: "tiny",
-  },
-});
-
+/**
+ * Props for the AddressAvatar component.
+ * @property {string | null} [address] - The Ethereum address to generate an avatar for.
+ * @property {string | null} [email] - The email address to generate a Gravatar for.
+ * @property {boolean} [badge] - Whether to show a badge on the avatar.
+ * @property {'big' | 'small'} [variant='big'] - The size variant of the avatar.
+ */
 interface AddressAvatarProps extends HTMLAttributes<HTMLDivElement> {
   address?: string | null;
   email?: string | null;
@@ -27,46 +23,76 @@ interface AddressAvatarProps extends HTMLAttributes<HTMLDivElement> {
   variant?: "big" | "small" | "tiny";
 }
 
-async function getEnsAvatarUrl(address?: string | null) {
-  if (address) {
-    try {
-      const publicClient = createPublicClient({
-        chain: mainnet,
-        transport: http(),
-      });
-
-      const ensName = await publicClient.getEnsName({
-        address: address as Address,
-      });
-
-      if (ensName) {
-        return `https://metadata.ens.domains/mainnet/avatar/${ensName}`;
-      }
-    } catch (error) {
-      console.debug(error);
-      return undefined;
-    }
-  }
-
-  return undefined;
-}
-
 /**
  * AddressAvatar component displays an avatar based on the provided address or email.
+ * It shows a skeleton loader while the avatar is being fetched and fades in the actual image when loaded.
+ *
+ * @param {AddressAvatarProps} props - The component props.
+ * @param {React.Ref<HTMLDivElement>} ref - The ref to be forwarded to the root element.
+ * @returns {JSX.Element} The rendered AddressAvatar component.
  */
-export async function AddressAvatar({ address, email, className, indicator, variant, ...props }: AddressAvatarProps) {
-  const ensAvatar = await getEnsAvatarUrl(address);
-  const gravatarAvatar = getGravatarUrl(email ?? "", {
-    default: "identicon",
-    size: 400,
-  });
+export const AddressAvatar = forwardRef<HTMLDivElement, AddressAvatarProps>(
+  ({ address, email, className, indicator, variant = "big", ...props }, ref) => {
+    const { data: avatar } = useSuspenseQuery({
+      queryKey: ["avatar", email, address],
+      queryFn: async () => {
+        if (address) {
+          const publicClient = createPublicClient({
+            chain: mainnet,
+            transport: http("https://eth.merkle.io"),
+          });
 
-  return (
-    <div {...props} className={cn(addressAvatarVariants({ variant }), className, "relative")}>
-      <Avatar className={cn(addressAvatarVariants({ variant }), className)}>
-        <AvatarImage src={ensAvatar ?? gravatarAvatar} alt={email ?? address ?? ""} />
-      </Avatar>
-      {indicator && <AddressAvatarIndicator variant={variant} />}
-    </div>
-  );
-}
+          const ensName = await publicClient.getEnsName({
+            address: address as Address,
+          });
+
+          if (ensName) {
+            return { ensName, avatar: `https://metadata.ens.domains/mainnet/avatar/${ensName}` };
+          }
+        }
+        if (email) {
+          const avatarUrl = getGravatarUrl(email ?? "", {
+            default: "identicon",
+            size: 400,
+          });
+
+          return { avatar: avatarUrl };
+        }
+        return null;
+      },
+    });
+
+    const [imageLoaded, setImageLoaded] = useState(false);
+
+    return (
+      <div className="relative">
+        <Avatar
+          ref={ref}
+          className={cn(variant === "big" ? "w-10 h-10" : variant === "small" ? "w-6 h-6" : "w-4 h-4", className)}
+          {...props}
+        >
+          <AvatarFallback
+            className={cn(variant === "big" ? "w-10 h-10" : variant === "small" ? "w-6 h-6" : "w-4 h-4", className)}
+          >
+            <Skeleton className={cn("rounded-full absolute inset-0", imageLoaded ? "opacity-0" : "opacity-100")} />
+          </AvatarFallback>
+          {avatar?.avatar && (
+            <AvatarImage
+              src={avatar.avatar}
+              className={cn("transition-opacity duration-300", imageLoaded ? "opacity-100" : "opacity-0")}
+              onLoad={() => setImageLoaded(true)}
+            />
+          )}
+        </Avatar>
+        {indicator && (
+          <span className="absolute -top-0.5 -right-0.5  flex h-3 w-3">
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary opacity-75" />
+            <span className="relative inline-flex rounded-full h-3 w-3 bg-primary" />
+          </span>
+        )}
+      </div>
+    );
+  },
+);
+
+AddressAvatar.displayName = "AddressAvatar";
