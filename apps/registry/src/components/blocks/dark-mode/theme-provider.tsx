e621ba16fb040d4bf9dabecce30583414e5a4f9d
Roderik van der Veer <roderik@settlemint.com>
2024-10-26 09:14:21 +0200

    fix: improve hydration errors

--- apps/registry/src/components/blocks/dark-mode/theme-provider.tsx
+++ apps/registry/src/components/blocks/dark-mode/theme-provider.tsx
@@ -1,7 +1,12 @@
 "use client";
 
-import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { ThemeProviderProps } from "next-themes/dist/types";
+import dynamic from "next/dynamic";
+
+// Fixes hydration error
+const NextThemesProvider = dynamic(() => import("next-themes").then((e) => e.ThemeProvider), {
+  ssr: false,
+});
 
 /**
  * A wrapper component that provides theme functionality to its children.
