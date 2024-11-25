f8b429e59fbe948f51f99cfd50456b2375dd672e
Roderik van der Veer <roderik@settlemint.com>
2024-11-25 16:37:15 +0100

    feat: add barebones starterkit

---
+++ apps/starterkit-asset-tokenization/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  experimental: {
+    reactCompiler: true,
+  },
+  output: "standalone",
+};
+
+export default nextConfig;
