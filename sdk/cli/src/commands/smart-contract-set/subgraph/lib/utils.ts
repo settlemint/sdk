a837244f0f90654d9a2e3969ed767b128645cbe2
janb87 <12234016+janb87@users.noreply.github.com>
2024-12-06 16:52:45 +0100

    subgraphs in separate pr

--- sdk/cli/src/commands/smart-contract-set/subgraph/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { parse } from "yaml";
-import type { SubgraphConfig } from "./common.js";
-import { isGenerated } from "./is-generated.js";
-export const subgraphYamlFile = isGenerated ? "generated/scs.subgraph.yaml" : "subgraph/subgraph.yaml";
-
-export const getSubgraphYamlConfig = async (): Promise<SubgraphConfig> => {
-  const rawYamlConfig = await Bun.file(subgraphYamlFile).text();
-  return parse(rawYamlConfig);
-};
