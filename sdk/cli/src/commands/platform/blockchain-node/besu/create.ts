de3f7d9794eb2def6b586b88b130c7d7737d20ea
Snigdha Singh <snigdha920@gmail.com>
2024-12-20 10:30:50 +0100

    choose network if not present in the env

--- sdk/cli/src/commands/platform/blockchain-node/besu/create.ts
+++ sdk/cli/src/commands/platform/blockchain-node/besu/create.ts
@@ -1,7 +1,18 @@
+import { blockchainNetworkPrompt } from "@/commands/connect/blockchain-network.prompt";
 import { addClusterServiceArgs } from "@/commands/platform/common/cluster-service.args";
 import { Option } from "@commander-js/extra-typings";
 import type { DotEnv } from "@settlemint/sdk-utils";
+import { cancel } from "@settlemint/sdk-utils/terminal";
 import { getCreateCommand } from "../../common/create-command";
+
+const prettyNodeTypeNames = {
+  validator: "VALIDATOR",
+  "non-validator": "NON_VALIDATOR",
+  notary: "NOTARY",
+  orderer: "ORDERER",
+  peer: "PEER",
+  unspecified: "UNSPECIFIED",
+} as const;
 
 /**
  * Creates and returns the 'blockchain-node besu' command for the SettleMint SDK.
@@ -20,11 +31,11 @@ export function blockchainNodeBesuCreateCommand() {
           "-a, --application-id <applicationId>",
           "The application ID to create the node in (defaults to application from env)",
         )
-        .requiredOption("--blockchain-network-id <blockchainNetworkId>", "Blockchain network to add this node to")
+        .option("--blockchain-network-id <blockchainNetworkId>", "Blockchain network to add this node to")
         .option("--node-identity <nodeIdentity>", "EC DSA P256 private key to use as the node identity")
         .addOption(
           new Option("--node-type <nodeType>", "Type of the node")
-            .choices(["VALIDATOR", "NON_VALIDATOR", "NOTARY", "ORDERER", "PEER", "UNSPECIFIED"] as const)
+            .choices(Object.keys(prettyNodeTypeNames) as (keyof typeof prettyNodeTypeNames)[])
             .makeOptionMandatory(),
         )
         .action(
@@ -39,16 +50,31 @@ export function blockchainNodeBesuCreateCommand() {
               blockchainNetworkId,
               nodeType,
               nodeIdentity,
+              acceptDefaults,
               ...defaultArgs
             },
           ) => {
             return baseAction(defaultArgs, async (settlemint, env) => {
               const application = applicationId ?? env.SETTLEMINT_APPLICATION!;
+              if (!application) {
+                cancel("No application found");
+              }
+
+              let networkId = blockchainNetworkId ?? env.SETTLEMINT_BLOCKCHAIN_NETWORK!;
+              if (!networkId) {
+                const networks = await settlemint.blockchainNetwork.list(application);
+                const network = await blockchainNetworkPrompt(env, networks, acceptDefaults ?? false);
+                if (!network) {
+                  cancel("No network found");
+                }
+                networkId = network?.id;
+              }
+
               const result = await settlemint.blockchainNode.create({
                 applicationId: application,
                 name,
-                blockchainNetworkId,
-                nodeType,
+                blockchainNetworkId: networkId,
+                nodeType: prettyNodeTypeNames[nodeType],
                 keyMaterial: nodeIdentity,
                 provider,
                 region,
@@ -72,12 +98,17 @@ export function blockchainNodeBesuCreateCommand() {
     examples: [
       {
         description: "Create a Besu blockchain node and save as default",
-        command: "platform create blockchain-node besu my-node --node-name validator-1 --accept-defaults -d",
+        command: "platform create blockchain-node besu my-node --node-type validator --accept-defaults -d",
+      },
+      {
+        description: "Create a Besu blockchain node in a different network",
+        command:
+          "platform create blockchain-node besu my-node --blockchain-network-id 12345 --node-type non-validator --accept-defaults",
       },
       {
         description: "Create a Besu blockchain node in a different application",
         command:
-          "platform create blockchain-node besu my-node --application-id 123456789 --node-name validator-1 --chain-id 12345 --gas-limit 10000000 --seconds-per-block 5",
+          "platform create blockchain-node besu my-node --application-id 123456789 --node-type non-validator --accept-defaults",
       },
     ],
   });
