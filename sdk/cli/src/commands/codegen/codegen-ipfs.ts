b27677708f24d9bc3ffaa7e7b35e48b237d33e4c
Snigdha Singh <62167899+snigdha920@users.noreply.github.com>
2024-12-21 15:25:49 +0100

    fix: ipfs, minio instance endpoints in codegen (#528)
    ## Summary by Sourcery
    
    Bug Fixes:
    - Fixed incorrect environment variables being used for IPFS and Minio
    client generation.

--- sdk/cli/src/commands/codegen/codegen-ipfs.ts
+++ sdk/cli/src/commands/codegen/codegen-ipfs.ts
@@ -14,7 +14,7 @@ export async function codegenIpfs(env: DotEnv) {
   const clientTemplate = `import { createServerIpfsClient } from "@settlemint/sdk-ipfs";
 
 export const { client } = createServerIpfsClient({
-  instance: process.env.SETTLEMINT_HASURA_ENDPOINT!,
+  instance: process.env.SETTLEMINT_IPFS_API_ENDPOINT!,
   accessToken: process.env.SETTLEMINT_ACCESS_TOKEN!,
 });`;
 
