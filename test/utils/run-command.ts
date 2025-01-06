a416fca08a39d5b4194d8c3d1dccecd8e9d08a9f
janb87 <12234016+janb87@users.noreply.github.com>
2025-01-06 10:59:46 +0100

    only set env

--- test/utils/run-command.ts
+++ test/utils/run-command.ts
@@ -8,7 +8,9 @@ import { isLocalEnv } from "./is-local-env";
 const commandsRunning: Record<string, ChildProcessWithoutNullStreams[]> = {};
 
 const DEFAULT_ENV: Record<string, string> = {
-  ...(Object.fromEntries(Object.entries(process.env).filter(([_, value]) => value !== "")) as Record<string, string>),
+  ...(Object.fromEntries(
+    Object.entries(process.env).filter(([_, value]) => typeof value === "string" && value !== ""),
+  ) as Record<string, string>),
   SETTLEMINT_ACCESS_TOKEN: process.env.SETTLEMINT_ACCESS_TOKEN!,
   SETTLEMINT_INSTANCE: process.env.SETTLEMINT_INSTANCE!,
   CI: isInCi ? "true" : "false",
