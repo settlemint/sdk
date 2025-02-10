2ddab7bccbc66808910a6ba1b519c317285e486f
janb87 <12234016+janb87@users.noreply.github.com>
2025-02-10 15:03:11 +0100

    fix: passthrough of '-v' option on commands like forge test

---
+++ sdk/cli/src/commands/smart-contract-set/foundry/test.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect, test } from "bun:test";
+import { mapPassthroughOptions } from "@/utils/commands/passthrough-options";
+import { Command } from "@commander-js/extra-typings";
+import { foundryTestCommand } from "./test";
+
+describe("foundryTestCommand", () => {
+  test("executes test command with vvv option", () => {
+    let commandOptions: string[] = [];
+    const program = new Command();
+    program.addCommand(
+      foundryTestCommand().action((passThroughOptions, cmd) => {
+        const forgeOptions = mapPassthroughOptions(passThroughOptions, cmd);
+        commandOptions = forgeOptions;
+      }),
+    );
+    program.parse(["node", "test", "test", "-vvv"]);
+
+    expect(commandOptions).toEqual(["-vvv"]);
+  });
+});
