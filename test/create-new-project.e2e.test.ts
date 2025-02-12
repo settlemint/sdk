3d18923bf32d9c5311b7802a66db1e6cafe3b28d
janb87 <12234016+janb87@users.noreply.github.com>
2025-02-12 10:03:12 +0100

    fix: e2e tests

--- test/create-new-project.e2e.test.ts
+++ test/create-new-project.e2e.test.ts
@@ -128,36 +128,39 @@ describe("Setup a project using the SDK", () => {
 
   test("contracts - Build and Deploy smart contracts", async () => {
     const deploymentId = "starterkit-asset-tokenization";
-    // Only deploy the stable coin factory, otherwise it will take very long to deploy all the contracts
-    const { output: deployOutput } = await retryCommand(
-      () =>
-        runCommand(
-          COMMAND_TEST_SCOPE,
-          [
-            "scs",
-            "hardhat",
-            "deploy",
-            "remote",
-            "--deployment-id",
-            deploymentId,
-            "--module",
-            "ignition/modules/stable-coin-factory.ts",
-            "--accept-defaults",
-          ],
-          {
-            cwd: contractsDir,
-            env: {
-              HARDHAT_IGNITION_CONFIRM_DEPLOYMENT: "false",
+    // Only deploy the forwareder, otherwise it will take very long to deploy all the contracts
+    const ignitionModules = ["ignition/modules/forwarder.ts", "ignition/modules/stable-coin-factory.ts"];
+    for (const ignitionModule of ignitionModules) {
+      const { output: deployOutput } = await retryCommand(
+        () =>
+          runCommand(
+            COMMAND_TEST_SCOPE,
+            [
+              "scs",
+              "hardhat",
+              "deploy",
+              "remote",
+              "--deployment-id",
+              deploymentId,
+              "--module",
+              ignitionModule,
+              "--accept-defaults",
+            ],
+            {
+              cwd: contractsDir,
+              env: {
+                HARDHAT_IGNITION_CONFIRM_DEPLOYMENT: "false",
+              },
             },
-          },
-        ).result,
-    );
-    const deploymentInfoData = await readFile(
-      join(contractsDir, "ignition", "deployments", deploymentId, "deployed_addresses.json"),
-    );
-    contractsDeploymentInfo = JSON.parse(deploymentInfoData.toString());
-    expect(deployOutput).toInclude("successfully deployed 🚀");
-    expect(deployOutput).not.toInclude("Error reading hardhat.config.ts");
+          ).result,
+      );
+      const deploymentInfoData = await readFile(
+        join(contractsDir, "ignition", "deployments", deploymentId, "deployed_addresses.json"),
+      );
+      contractsDeploymentInfo = JSON.parse(deploymentInfoData.toString());
+      expect(deployOutput).toInclude("successfully deployed 🚀");
+      expect(deployOutput).not.toInclude("Error reading hardhat.config.ts");
+    }
   });
 
   test("subgraph - Update contract addresses", async () => {
