824e1022c5f2b3849055179f817a9c9059428f92
Snigdha Singh <snigdha920@gmail.com>
2025-03-21 14:03:20 +0100

    cleanup logging

--- sdk/cli/src/commands/hasura/track.ts
+++ sdk/cli/src/commands/hasura/track.ts
@@ -19,7 +19,7 @@ export function hasuraTrackCommand() {
     .usage(
       createExamples([
         {
-          description: "Track all tables in all schemas",
+          description: "Track all tables",
           command: "hasura track",
         },
         {
