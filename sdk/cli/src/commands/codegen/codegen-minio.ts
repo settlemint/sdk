55908cb0818705237aef279c3998df619ac25ca2
janb87 <12234016+janb87@users.noreply.github.com>
2025-04-17 15:29:39 +0200

    feat: add minio e2e test

--- sdk/cli/src/commands/codegen/codegen-minio.ts
+++ sdk/cli/src/commands/codegen/codegen-minio.ts
@@ -19,7 +19,6 @@ export async function codegenMinio(env: DotEnv) {
 
 export const { client } = createServerMinioClient({
   instance: process.env.SETTLEMINT_MINIO_ENDPOINT!,
-  accessToken: process.env.SETTLEMINT_ACCESS_TOKEN!,
   accessKey: process.env.SETTLEMINT_MINIO_ACCESS_KEY!,
   secretKey: process.env.SETTLEMINT_MINIO_SECRET_KEY!
 });`;
