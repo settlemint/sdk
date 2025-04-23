1afe6e1d8332bf3997cdab1896d30ad63542d046
janb87 <12234016+janb87@users.noreply.github.com>
2025-04-23 17:30:44 +0200

    add tests

--- sdk/viem/src/viem.ts
+++ sdk/viem/src/viem.ts
@@ -1,32 +1,37 @@
 import {
   http,
-  type Chain,
   type HttpTransportConfig,
+  type Chain as ViemChain,
+  type Transport as ViemTransport,
   type WalletClient,
   createPublicClient,
   createWalletClient,
   defineChain,
 } from "viem";
-import chains from "viem/chains";
+import * as chains from "viem/chains";
 
 /**
  * The options for the viem client.
  */
 export interface ClientOptions {
   /**
-   * The chain id to use for the viem client.
+   * The access token
+   */
+  accessToken: string;
+  /**
+   * The chain id
    */
   chainId: string;
   /**
-   * The chain name to use for the viem client.
+   * The chain name
    */
   chainName: string;
   /**
-   * The rpc url to use for the viem client.
+   * The json rpc url
    */
   rpcUrl: string;
   /**
-   * The http transport config to use for the wallet client.
+   * The http transport config
    */
   httpTransportConfig?: HttpTransportConfig;
 }
@@ -35,6 +40,21 @@ export interface ClientOptions {
  * Get a public client. Use this if you need to read from the blockchain.
  * @param options - The options for the public client.
  * @returns The public client.
+ * @example
+ * ```ts
+ * import { getPublicClient } from '@settlemint/sdk-viem';
+ *
+ * const publicClient = getPublicClient({
+ *   accessToken: env.SETTLEMINT_ACCESS_TOKEN!,
+ *   chainId: env.SETTLEMINT_BLOCKCHAIN_NETWORK_CHAIN_ID!,
+ *   chainName: env.SETTLEMINT_BLOCKCHAIN_NETWORK!,
+ *   rpcUrl: env.SETTLEMINT_BLOCKCHAIN_NODE_OR_LOAD_BALANCER_JSON_RPC_ENDPOINT!,
+ * });
+ *
+ * // Get the block number
+ * const block = await publicClient.getBlockNumber();
+ * console.log(block);
+ * ```
  */
 export const getPublicClient = (options: ClientOptions) =>
   createPublicClient({
@@ -43,6 +63,13 @@ export const getPublicClient = (options: ClientOptions) =>
       batch: true,
       timeout: 60_000,
       ...options.httpTransportConfig,
+      fetchOptions: {
+        ...options?.httpTransportConfig?.fetchOptions,
+        headers: {
+          ...options?.httpTransportConfig?.fetchOptions?.headers,
+          "x-auth-token": options.accessToken,
+        },
+      },
     }),
   });
 
@@ -64,12 +91,38 @@ export interface WalletVerificationOptions {
  * Get a wallet client. Use this if you need to write to the blockchain.
  * @param options - The options for the wallet client.
  * @returns A function that returns a wallet client. The function can be called with verification options.
+ * @example
+ * ```ts
+ * import { getWalletClient } from '@settlemint/sdk-viem';
+ * import { parseAbi } from "viem";
+ *
+ * const walletClient = getWalletClient({
+ *   accessToken: env.SETTLEMINT_ACCESS_TOKEN!,
+ *   chainId: env.SETTLEMINT_BLOCKCHAIN_NETWORK_CHAIN_ID!,
+ *   chainName: env.SETTLEMINT_BLOCKCHAIN_NETWORK!,
+ *   rpcUrl: env.SETTLEMINT_BLOCKCHAIN_NODE_OR_LOAD_BALANCER_JSON_RPC_ENDPOINT!,
+ * });
+ *
+ * // Get the chain id
+ * const chainId = await walletClient().getChainId();
+ * console.log(chainId);
+ *
+ * // write to the blockchain
+ * const transactionHash = await walletClient().writeContract({
+ *   account: "0x0000000000000000000000000000000000000000",
+ *   address: "0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2",
+ *   abi: parseAbi(["function mint(uint32 tokenId) nonpayable"]),
+ *   functionName: "mint",
+ *   args: [69420],
+ * });
+ * console.log(transactionHash);
+ * ```
  */
-export const getWalletClient = (options: ClientOptions) => {
+export const getWalletClient = <C extends ViemChain>(options: ClientOptions) => {
   const chain = getChain(options);
-  return (verificationOptions?: WalletVerificationOptions): WalletClient =>
+  return (verificationOptions?: WalletVerificationOptions): WalletClient<ViemTransport, C> =>
     createWalletClient({
-      chain,
+      chain: chain as ViemChain,
       transport: http(options.rpcUrl, {
         batch: true,
         timeout: 60_000,
@@ -78,15 +131,16 @@ export const getWalletClient = (options: ClientOptions) => {
           ...options?.httpTransportConfig?.fetchOptions,
           headers: {
             ...options?.httpTransportConfig?.fetchOptions?.headers,
+            "x-auth-token": options.accessToken,
             "x-auth-challenge-response": verificationOptions?.challengeResponse ?? "",
             "x-auth-verification-id": verificationOptions?.verificationId ?? "",
           },
         },
       }),
-    });
+    }) as WalletClient<ViemTransport, C>;
 };
 
-function getChain({ chainId, chainName, rpcUrl }: Pick<ClientOptions, "chainId" | "chainName" | "rpcUrl">): Chain {
+function getChain({ chainId, chainName, rpcUrl }: Pick<ClientOptions, "chainId" | "chainName" | "rpcUrl">): ViemChain {
   const knownChain = Object.values(chains).find((chain) => chain.id.toString() === chainId);
   return (
     knownChain ??
