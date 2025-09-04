13a8d9df142a535b890ce45665eac5b7772ea5f1
Jan Bevers <jan@settlemint.com>
2025-09-04 14:46:21 +0200

    review

--- sdk/cli/src/commands/platform/blockchain-network/besu/create.ts
+++ sdk/cli/src/commands/platform/blockchain-network/besu/create.ts
@@ -31,7 +31,7 @@ export function blockchainNetworkBesuCreateCommand() {
         .option("--gas-limit <limit>", "Block gas limit")
         .option("--gas-price <price>", "Gas price in wei", parseNumber)
         .option("--seconds-per-block <seconds>", "Block time in seconds", parseNumber)
-        .option("--includePredeployedContracts", "Include predeployed contracts in the genesis file", parseNumber)
+        .option("--includePredeployedContracts", "Include predeployed contracts in the genesis file")
         .action(
           async (
             name,
