fe238e59f2afad765283a035f4ec656fdcb2cb1e
Jan Bevers <jan@settlemint.com>
2025-10-28 09:43:46 +0100

    fix: broken e2e tests

--- test/restart-resources.e2e.test.ts
+++ test/restart-resources.e2e.test.ts
@@ -1,8 +1,11 @@
 import { afterEach, describe, expect, setDefaultTimeout, test } from "bun:test";
+import { createSettleMintClient } from "@settlemint/sdk-js";
 import { loadEnv } from "@settlemint/sdk-utils/environment";
 import { fetchWithRetry } from "@settlemint/sdk-utils/http";
+import type { DotEnv } from "@settlemint/sdk-utils/validation";
 import { NODE_NAME_3_WITHOUT_PK } from "./constants/test-resources";
 import { forceExitAllCommands, runCommand } from "./utils/run-command";
+import { setupSettleMintClient } from "./utils/test-resources";
 
 const COMMAND_TEST_SCOPE = __filename;
 
@@ -14,11 +17,15 @@ afterEach(() => {
 
 describe("Restart platform resources using the SDK", () => {
   test("Restart blockchain node on the platform", async () => {
+    const blockchainNode = await findBlockchainNodeByName(NODE_NAME_3_WITHOUT_PK);
+    if (!blockchainNode) {
+      throw new Error(`Blockchain node ${NODE_NAME_3_WITHOUT_PK} not found`);
+    }
     const { output } = await runCommand(COMMAND_TEST_SCOPE, [
       "platform",
       "restart",
       "blockchain-node",
-      NODE_NAME_3_WITHOUT_PK,
+      blockchainNode.uniqueName,
       "--wait",
       "--accept-defaults",
     ]).result;
@@ -30,3 +37,13 @@ describe("Restart platform resources using the SDK", () => {
     expect(response.status).toBe(401); // Unauthorized as we did not provide a token
   });
 });
+
+async function findBlockchainNodeByName(blockchainNodeName: string) {
+  const env: Partial<DotEnv> = await loadEnv(false, false);
+  const settlemint = createSettleMintClient({
+    accessToken: env.SETTLEMINT_ACCESS_TOKEN!,
+    instance: env.SETTLEMINT_INSTANCE!,
+  });
+  const nodes = await settlemint.blockchainNode.list(env.SETTLEMINT_APPLICATION!);
+  return nodes.find((node) => node.name === blockchainNodeName);
+}
